1586d36a26453868b1d10d21ee5e85952bc8cacb
mdapkus <dapkui.mantui@gmail.com>
2019-12-13 15:22:54 +0200

    Finished the project.

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -16,7 +16,7 @@ namespace Homework1
             Console.Write("Įveskite sveikąjį skaičių: ");
             string input = Console.ReadLine();
 
-            if (!CheckIfIsInteger(input))
+            if (!IsInteger(input))
             {
                 Console.WriteLine(input + " nėra sveikasis skaičius. Spauskite Enter jeigu norite tęsti.");
                 Console.ReadLine();
@@ -31,11 +31,12 @@ namespace Homework1
                 Console.ReadLine();
                 goto beginning;
             }
+
             string answer = ChangeNumberToText(number).Trim();
             answer = char.ToUpper(answer[0]) + answer.Substring(1);
             Console.WriteLine(answer);
         }
-        static bool CheckIfIsInteger(string input)
+        static bool IsInteger(string input)
         {
             input = input.Trim();
             bool qualifiesToInteger = false;
@@ -68,6 +69,91 @@ namespace Homework1
         }
         static string ChangeNumberToText(int number)
         {
+            Dictionary<int, string> dictNumbersToWords = CreateDictionaryNumberToWords();
+
+            string numberToText = string.Empty;
+            if (number < 0)
+            {
+                numberToText = "minus";
+            }
+
+            number = Math.Abs(number);
+
+            if (number == 0)
+            {
+                return "Nulis";
+            }
+
+            if (number >= 100000000) //100 000 000 - 999 999 999
+            {
+                numberToText = HundredsToText(number, numberToText, "milijonu", dictNumbersToWords);
+                CutFirstDigitFromNumber(ref number);
+            }
+
+            if (number >= 10000000) //10 000 000 - 99 999 999
+            {
+                numberToText = TensToText(number, numberToText, dictNumbersToWords, "milijonu", "milijonas", "milijonai");
+                CutFirstDigitFromNumber(ref number);
+                if (number != 0)
+                {
+                    CutFirstDigitFromNumber(ref number);
+                }
+            }
+
+            if (number >= 1000000) //1 000 000 - 9 999 999
+            {
+                numberToText = UnitsToText(number, numberToText, dictNumbersToWords, "milijonas", "milijonai");
+                CutFirstDigitFromNumber(ref number);
+            }
+
+            if (number >= 100000) //100 000 - 999 999
+            {
+                numberToText = HundredsToText(number, numberToText, "tukstanciu", dictNumbersToWords);
+                CutFirstDigitFromNumber(ref number);
+            }
+
+            if (number >= 10000) //10 000 - 99 999
+            {
+                numberToText = TensToText(number, numberToText, dictNumbersToWords, "tukstanciu", "tukstantis", "tukstanciai");
+                CutFirstDigitFromNumber(ref number);
+                if (number != 0)
+                {
+                    CutFirstDigitFromNumber(ref number);
+                }
+            }
+
+            if (number >= 1000) //1000-9999
+            {
+                numberToText = UnitsToText(number, numberToText, dictNumbersToWords, "tukstantis", "tukstanciai");
+
+                CutFirstDigitFromNumber(ref number);
+            }
+
+            if (number >= 100) // 100-999
+            {
+                numberToText = HundredsToText(number, numberToText, string.Empty, dictNumbersToWords);
+                CutFirstDigitFromNumber(ref number);
+            }
+
+            if (number >= 10)
+            {
+                numberToText = TensToText(number, numberToText, dictNumbersToWords, string.Empty, string.Empty, string.Empty);
+                CutFirstDigitFromNumber(ref number);
+                if (number != 0)
+                {
+                    CutFirstDigitFromNumber(ref number);
+                }
+            }
+
+            if (number < 10)
+            {
+                numberToText = UnitsToText(number, numberToText, dictNumbersToWords, "vienas", string.Empty);
+            }
+
+            return numberToText;
+        }
+        static Dictionary<int, string> CreateDictionaryNumberToWords()
+        {
             Dictionary<int, string> dictNumbersToWords = new Dictionary<int, string>();
             dictNumbersToWords.Add(0, string.Empty);
             dictNumbersToWords.Add(1, "vienas");
@@ -97,225 +183,73 @@ namespace Homework1
             dictNumbersToWords.Add(70, "septyniasdešimt");
             dictNumbersToWords.Add(80, "aštuoniasdešimt");
             dictNumbersToWords.Add(90, "devyniasdešimt");
-            int firstDigit = 0;
-            int secondDigit = 0;
-            int thirdDigit = 0;
-            string suffix = string.Empty;
-
-            string numberToText = string.Empty;
-            if (number < 0)
+            return dictNumbersToWords;
+        }
+        static string HundredsToText(int number, string numberToText, string suffix, Dictionary<int, string> dictNumbersToWords)
+        {
+            int firstDigit = GetFirstDigit(number);
+            if (firstDigit == 1)
             {
-                numberToText = "minus";
+                numberToText = numberToText + " simtas";
             }
-            number = Math.Abs(number);
-            if (number == 0)
+            else
             {
-                return "Nulis";
+                numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit], " šimtai");
             }
-
-            if (number >= 100000000) //100 000 000 - 999 999 999
+            if (GetFirstThreeNumbers(number).ToString().EndsWith("00"))
             {
-                firstDigit = GetFirstDigit(number);
-                if (firstDigit == 1)
-                {
-                    numberToText = numberToText + " simtas";
-                }
-                else
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit]," šimtai");
-                }
-                if (GetFirstThreeNumbers(number).ToString().EndsWith("00"))
-                {
-                    numberToText = String.Concat(numberToText, " ", " milijonu");
-                }
-                CutFirstDigitFromNumber(ref number);
+                numberToText = String.Concat(numberToText, " ", suffix);
             }
-            if (number >= 10000000) //10 000 000 - 99 999 999
-            {
-                string target = string.Empty;
-                firstDigit = GetFirstDigit(number);
-                secondDigit = GetSecondDigit(number);
-                suffix = "milijonu";
-                if (firstDigit == 1)
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[GetFirstTwoNumbers(number)], " ", suffix);
-                }
-                else 
-                {
-                    if (secondDigit == 1)
-                    {
-                        suffix = "milijonas";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
-                    }
-                    else if(secondDigit ==0)
-                    {
-                        suffix = "milijonu";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)];
-                    }
-                    else
-                    {
-                        suffix = "milijonai";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
-                    }
-
-                    numberToText = String.Concat(numberToText, " ", target, " ", suffix);
-                }
-
-                CutFirstDigitFromNumber(ref number);
-                if (number != 0)
-                {
-                    CutFirstDigitFromNumber(ref number);
-                }
-            }
-            if (number >= 1000000) //1 000 000 - 9 999 999
-            {
-                firstDigit = GetFirstDigit(number);
-                if (firstDigit == 1)
-                {
-                    numberToText = String.Concat(numberToText, " milijonas");
-
-                }
-                else
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit] + " milijonai");
-                }
-                CutFirstDigitFromNumber(ref number);
-
-            }
-            if (number >= 100000) //100 000 - 999 999
-            {
-                firstDigit = GetFirstDigit(number);
-                if (firstDigit == 1)
-                {
-                    numberToText = numberToText + " simtas";
-                }
-                else
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit], " šimtai");
-                }
-                if (GetFirstThreeNumbers(number).ToString().EndsWith("00"))
-                {
-                    numberToText = String.Concat(numberToText, " ", " tukstanciu");
-                }
-                CutFirstDigitFromNumber(ref number);
-            }
-            if (number >= 10000) //10 000 - 99 999
-            {
-                string target = string.Empty;
-                firstDigit = GetFirstDigit(number);
-                secondDigit = GetSecondDigit(number);
-                suffix = "tukstanciu";
-                if (firstDigit == 1)
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[GetFirstTwoNumbers(number)], " ", suffix);
-                }
-                else
-                {
-                    if (secondDigit == 1)
-                    {
-                        suffix = "tukstantis";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
-                    }
-                    else if (secondDigit == 0)
-                    {
-                        suffix = "tukstanciu";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)];
-                    }
-                    else
-                    {
-                        suffix = "tukstanciai";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
-                    }
-
-                    numberToText = String.Concat(numberToText, " ", target, " ", suffix);
-                }
-
-                CutFirstDigitFromNumber(ref number);
-                if (number != 0)
-                {
-                    CutFirstDigitFromNumber(ref number);
-                }
-            }
-            if (number >= 1000) //1000-9999
-            {
-                firstDigit = GetFirstDigit(number);
-                if (firstDigit == 1)
-                {
-                    numberToText = String.Concat(numberToText, " tukstantis");
-
-                }
-                else
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit] + " tukstanciai");
-                }
-                CutFirstDigitFromNumber(ref number);
-            }
-            if (number >= 100) // 100-999
-            {
-                firstDigit = GetFirstDigit(number);
-                if (firstDigit == 1)
-                {
-                    numberToText = numberToText + " simtas";
-                }
-                else
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit], " šimtai");
-                }
-                CutFirstDigitFromNumber(ref number);
-            }
-            if ( number >= 10)
-            {
-                string target = string.Empty;
-                firstDigit = GetFirstDigit(number);
-                secondDigit = GetSecondDigit(number);
-                suffix = "";
-                if (firstDigit == 1)
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[GetFirstTwoNumbers(number)], " ", suffix);
-                }
-                else
-                {
-                    if (secondDigit == 1)
-                    {
-                        suffix = "";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
-                    }
-                    else if (secondDigit == 0)
-                    {
-                        suffix = "";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)];
-                    }
-                    else
-                    {
-                        suffix = "";
-                        target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
-                    }
-
-                    numberToText = String.Concat(numberToText, " ", target, " ", suffix);
-                }
-
-                CutFirstDigitFromNumber(ref number);
-                if (number != 0)
-                {
-                    CutFirstDigitFromNumber(ref number);
-                }
-
-            }
-            if (number < 10)
-            {
-                firstDigit = GetFirstDigit(number);
-                if (firstDigit == 1)
-                {
-                    numberToText = String.Concat(numberToText, " vienas");
-
-                }
-                else
-                {
-                    numberToText = String.Concat(numberToText, " ", dictNumbersToWords[firstDigit]);
-                }
-            }
-
             return numberToText;
+        }
+        static string TensToText(int number, string numberToText, Dictionary<int, string> dictNumbersToWords, string suffix0, string suffix1, string suffixOther)
+        {
+            string target = string.Empty;
+            string suffix = string.Empty;
+            int firstDigit = GetFirstDigit(number);
+            int secondDigit = GetSecondDigit(number);
+            if (firstDigit == 1)
+            {
+                suffix = suffix0;
+                return String.Concat(numberToText, " ", dictNumbersToWords[GetFirstTwoNumbers(number)], " ", suffix);
+            }
+            else
+            {
+                if (secondDigit == 1)
+                {
+                    suffix = suffix1;
+                    target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
+                }
+                else if (secondDigit == 0)
+                {
+                    suffix = suffix0;
+                    target = dictNumbersToWords[FloorBeginningToTens(firstDigit)];
+                }
+                else
+                {
+                    suffix = suffixOther;
+                    target = dictNumbersToWords[FloorBeginningToTens(firstDigit)] + " " + dictNumbersToWords[secondDigit];
+                }
+
+                return String.Concat(numberToText, " ", target, " ", suffix);
+            }
+        }
+        static string UnitsToText(int number, string numberToText, Dictionary<int, string> dictNumbersToWords, string suffix1, string suffixOther)
+        {
+            int firstDigit = GetFirstDigit(number);
+            if (!String.IsNullOrWhiteSpace(suffixOther))
+            {
+                suffixOther = " " + suffixOther;
+            }
+
+            if (firstDigit == 1)
+            {
+                return String.Concat(numberToText, " " + suffix1);
+            }
+            else
+            {
+                return String.Concat(numberToText, " ", dictNumbersToWords[firstDigit] + suffixOther);
+            }
         }
         static int GetFirstDigit(int number)
         {
@@ -325,16 +259,13 @@ namespace Homework1
         {
             return Convert.ToInt32(number.ToString()[1].ToString());
         }
-        static int FloorBeginningToHundreds(int firstNumber) {
-            return Convert.ToInt32(firstNumber.ToString() + "00");
-        }
         static int FloorBeginningToTens(int firstNumber)
         {
             return Convert.ToInt32(firstNumber.ToString() + "0");
         }
         static int GetFirstTwoNumbers(int number)
         {
-            return Convert.ToInt32(number.ToString().Substring(0,2));
+            return Convert.ToInt32(number.ToString().Substring(0, 2));
         }
         static int GetFirstThreeNumbers(int number)
         {
